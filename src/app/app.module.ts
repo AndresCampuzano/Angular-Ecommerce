94de6604c00e4254336f45542b3a527cb9b54d7c
Andres Campuzano <campuxano88@gmail.com>
2020-08-16 07:53:24 -0500

    Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MiComponente } from './components/mi-componente/mi-componente.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { PruebasComponent } from './components/pruebas/pruebas.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiComponente,
+    PeliculasComponent,
+    PruebasComponent,
+    HeaderComponent,
+    SliderComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
